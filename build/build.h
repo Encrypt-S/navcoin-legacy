320728b779988c9331ef132e072ea757f6e218d0
Craig <craig.b.macgregor@gmail.com>
2016-05-31 02:10:50 +1200

    coin swap wallet ready

--- build/build.h
+++ build/build.h
@@ -1,6 +1,2 @@
 // No build information available
-<<<<<<< HEAD
 #define BUILD_DATE "2016-01-29 02:26:01 +0530"
-=======
-#define BUILD_DATE "2016-05-01 19:57:20 +1200"
->>>>>>> coin-swap
--- src/obj/build.h
+++
@@ -1,2 +0,0 @@
-// No build information available
-#define BUILD_DATE "2016-05-14 17:46:23 +1200"
